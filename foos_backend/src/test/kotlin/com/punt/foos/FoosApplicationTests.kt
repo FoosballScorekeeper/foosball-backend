cbed24123bd3b3d464530f8053a00250bef0f722
Christopher Punt <43280249+christopherpunt@users.noreply.github.com>
2025-07-13 19:58:45 -0400

    backend initial project setup (#1)

---
+++ foos_backend/src/test/kotlin/com/punt/foos/FoosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.punt.foos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
